8bc569250bda4c59382d4894372011e06ad5fd03
Benit Havugimana <jajabenit@gmail.com>
2021-10-18 18:51:17 +0200

    setting up angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './core/first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first', component: FirstComponent },
+  { path: '',   redirectTo: '/first', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: FirstComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
