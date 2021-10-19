392c7f5b58d09893e19436c50a1982f1d89d2b95
Benit Havugimana <jajabenit@gmail.com>
2021-10-19 16:58:51 +0200

    configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SecondComponent } from './core/second/second.component';
 import { FirstComponent } from './core/first/first.component';
 
+
 const routes: Routes = [
-  { path: 'first', component: FirstComponent },
+  { path: 'first', component: SecondComponent,
+  children: [
+    {
+      path: 'child', // child route path
+      component: SecondComponent, // child route component that the router renders
+    },
+    {
+      path: 'child-b',
+      component: SecondComponent, // another child route component that the router renders
+    },
+  ], },
   { path: '',   redirectTo: '/first', pathMatch: 'full' }, // redirect to `first-component`
-  { path: '**', component: FirstComponent },  // Wildcard route for a 404 page
+  { path: '**', component: SecondComponent },  // Wildcard route for a 404 page
 ];
 
 @NgModule({
--- src/app/core/second/second.component.ts
+++ src/app/core/second/second.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-second',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
   }
